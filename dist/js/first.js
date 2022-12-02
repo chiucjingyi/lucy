7e64974c11d9ae295338a9080e3fe9762f36db76
My Name <qwed0979271519@gmail.com>
2022-12-03 02:49:04 +0800

    go11

---
+++ dist/js/first.js
@@ -0,0 +1,13 @@
+alert('hi');
+
+let n1 = 1;
+let n2 = 1.25;
+let n3 = '10';
+let n4 = n1 + n2;
+let n5 = n1 + n3;
+let n6 = parseInt(n3, 10) + n1
+
+
+console.log(n4);
+console.log(n5);
+console.log(n6);
