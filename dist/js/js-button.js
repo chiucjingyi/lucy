e889b324cc80ce374d9006837d02411baaf96ec8
My Name <qwed0979271519@gmail.com>
2022-12-08 12:27:10 +0800

    go9

---
+++ dist/js/js-button.js
@@ -0,0 +1,11 @@
+let test1 = document .querySelector('#click');
+let res = document .querySelector('#response');
+let na = document .querySelector('#na');
+console. log(test1);
+console. log(res);
+console. log(na);
+
+test1 .addEventListener ('click', function(){
+    response .innerHTML
+    }
+)
